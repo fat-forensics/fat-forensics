a2a5967f2963c7730a8ab2fde5be5fec829157e2
alexhepburn <ah13558@eduroam-209-82.nomadic.bris.ac.uk>
2018-09-04 15:50:36 +0100

    Added example test script in order to test if circleci integration is working

---
+++ fatf/tests/test_example.py
@@ -0,0 +1,8 @@
+# File names for all tests must begin with 'test_'
+# pytest documentation can provide lots of examples with how to construct test scripts
+
+def should_fail():
+     assert(0==1)
+
+def should_pass():
+     assert(0==0)

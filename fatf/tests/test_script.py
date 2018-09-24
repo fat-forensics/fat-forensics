4598b471f27af7c68fcf11ec3bc83dd0c871a570
mattclifford1 <mattc863@hotmail.com>
2018-09-24 15:21:00 +0100

    test script updated to pass linting

--- fatf/tests/test_script.py
+++ fatf/tests/test_script.py
@@ -1,15 +1,28 @@
-def times(x, y):
-    return x*y
+''' script for testing purposes '''
+
+
+def times(x_1, y_2):
+    ''' times together '''
+    return x_1*y_2
+
 
 def test_pass():
+    ''' designed to pass '''
     assert times(1, 2) == 2
 
+# leave this function commented out for CI to pass
+# only uncomment to test making the CI fail
 # def test_fail():
-# 	assert times(1, 2) == 3
+#    ''' designed to fail '''
+#   assert times(1, 2) == 3
+
 
 def main():
-	test_pass()
+    ''' run fucntions '''
+    test_pass()
+    x = 1 # 
 
-#test coverage
+
+# test coverage
 if __name__ == '__main__':
-	main()
+    main()

22f42861d3f8a00674965d4536c86dc6932b5d8a
mattclifford1 <mattc863@hotmail.com>
2018-09-26 11:38:14 +0100

    fixed linting issues with tabs

--- fatf/tests/dummy_test.py
+++ fatf/tests/dummy_test.py
@@ -2,27 +2,27 @@
 
 
 def times(x_1, y_2):
- ''' times together '''
- return x_1*y_2
+    ''' times together '''
+    return x_1*y_2
 
 
 def test_pass():
- ''' designed to pass '''
- assert times(1, 2) == 2
+    ''' designed to pass '''
+    assert times(1, 2) == 2
 
 # leave this function commented out for CI to pass
 # only uncomment to test making the CI fail
 # def test_fail():
-#    ''' designed to fail '''
-#   assert times(1, 2) == 3
+#       ''' designed to fail '''
+#       assert times(1, 2) == 3
 
 
 def main():
- ''' run fucntions '''
- test_pass()
+    ''' run fucntions '''
+    test_pass()
 
 
 # test coverage
 if __name__ == '__main__':
- main()
+    main()
  

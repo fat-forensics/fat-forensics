50a36402480933a8bfd33f77144f259dcf09479b
RafaelPo <rp13102@bristol.ac.uk>
2018-12-06 14:05:35 +0000

    explainer

---
+++ fatf/interpretability/cf_explainer.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 11:48:46 2018
+
+@author: rp13102
+"""
+import numpy as np
+import itertools
+
+def combine_lists(list1, list2):
+    lists = [list1, list2]
+    list_lengths = [len(list1), len(list2)]
+    sorted_lengths = np.argsort(np.array(list_lengths))
+    min_val = list_lengths[sorted_lengths[0]]
+    longest_list = lists[sorted_lengths[1]]
+    newlist = []
+    for i in range(min_val):
+        newlist.append(lists[0][i])
+        newlist.append(lists[1][i])
+        
+    return newlist + longest_list[min_val:].tolist()
+
+class Explainer(object):
+    def __init__(self, 
+                 model,
+                 categorical_features,
+                 dataset,
+                 monotone = False,
+                 stepsizes = None,
+                 max_comb = 2,
+                 feature_ranges={},
+                 dist_funcs=None):
+        
+        self.model = model
+        self.monotone = monotone
+        self.dataset = dataset
+        self.categorical_features = categorical_features
+
+        default_stepsize = 1
+        if not feature_ranges:
+            self.get_feature_ranges()
+        else:
+            set0 = set(feature_ranges.keys())
+            set1 = set(self.dataset.dtype.names)
+            if not np.all(set0 == set1):
+                self.feature_ranges = feature_ranges
+                self.get_feature_ranges(tocomplete = list(set1.difference(set0)))
+                
+        self.max_comb = max_comb
+        if not stepsizes:
+            self.stepsizes = {}
+            for ftr in self.dataset.dtype.names:
+                if ftr not in self.categorical_features:
+                    self.stepsizes[ftr] = default_stepsize
+        set0 = set(stepsizes.keys())
+        set1 = set(self.dataset.dtype.names)
+        if not np.all(set0 == set1):
+            self.stepsizes = stepsizes
+            for ftr in list(set1.difference(set0)):
+                if ftr not in self.categorical_features:
+                    self.stepsizes[ftr] = default_stepsize
+        else:      
+            self.stepsizes = stepsizes
+
+        if not dist_funcs:
+            self.dist_funcs = {}
+            for ftr in self.dataset.dtype.names:
+                if ftr not in self.categorical_features:
+                    self.dist_funcs[ftr] = lambda x, y: np.abs(x - y)
+                else:
+                    self.dist_funcs[ftr] = lambda x, y: int(x != y)
+        else:
+            self.dist_funcs = dist_funcs
+    
+    def dist(self, v0, v1):
+        dist = 0
+        features = v0.dtype.names
+        for feature in features:
+            dist += self.dist_funcs[feature](v0[feature], v1[feature])
+        return dist  
+      
+    def get_feature_ranges(self, tocomplete = 'all'):
+        if tocomplete == 'all':
+            features_to_complete = self.dataset.dtype.names
+            self.feature_ranges = {}
+        else:
+            features_to_complete = tocomplete
+        for field_name in features_to_complete:
+            if field_name not in self.categorical_features:
+                self.feature_ranges[field_name] = (min(self.dataset[field_name]),
+                                              max(self.dataset[field_name]))
+            else:
+                self.feature_ranges[field_name] = np.unique(self.dataset[field_name])
+    
+    def modify_instance(self, x, ftrs, vals):
+        for idx, ftr in enumerate(ftrs):
+            x[ftr] = vals[idx]
+    
+    def get_value_combinations(self, ftr_combination):
+        list_of_values = []
+        for ftr in ftr_combination:
+            if ftr not in self.categorical_features:
+                s0 = np.arange(self.feature_ranges[ftr][0], 
+                               self.instance[ftr]-self.stepsizes[ftr], 
+                               self.stepsizes[ftr])
+                s1 = np.arange(self.instance[ftr] + self.stepsizes[ftr], 
+                               self.feature_ranges[ftr][1], 
+                               self.stepsizes[ftr])
+                combined = combine_lists(s0, s1)
+                list_of_values.append(combined)
+            else:
+                list_of_values.append(self.feature_ranges[ftr])
+        return itertools.product(*list_of_values)
+    
+    def pretty_print(self, all_scores):
+        order = np.argsort([item[1] for item in all_scores])
+        for idx in order:
+            instance = all_scores[idx][0]
+            sim = []
+            for ftr in self.ftrs:
+                if instance[ftr] == self.instance[ftr]:
+                    sim.append('_')
+                else:
+                    sim.append('*')
+            print('              ', sim)    
+            print('New instance: ', instance, '// Distance: ', all_scores[idx][1])
+            print('\n')
+            
+    def generate_counterfactual(self, 
+                                instance,
+                                target_class
+                                ):
+        self.target_class = target_class
+        self.instance = instance.copy(order='K')
+        self.ftrs = instance.dtype.names
+        ftrs_indices = list(range(len(self.ftrs)))
+        all_scores = []
+        for n in range(1, self.max_comb+1):            
+            ftr_combinations = list(itertools.combinations(ftrs_indices, n))
+            
+            for ftr_combination_indices in ftr_combinations:
+                scores = []
+                ftr_combination = [self.ftrs[item] for item in ftr_combination_indices]
+                value_combinations = self.get_value_combinations(ftr_combination)
+                for value_combination in value_combinations:
+                    test_instance = self.instance.copy(order='K')
+                    self.modify_instance(test_instance, 
+                                         ftr_combination, 
+                                         value_combination)
+                    pred = self.model.predict(np.array(test_instance.tolist(), dtype=float).reshape(1, -1))
+                    if pred[0] == self.target_class:
+                        scores.append((test_instance, self.dist(self.instance, test_instance)))
+                        if self.monotone:
+                            break
+                try:
+                    best = np.argmin([item[1] for item in scores])
+                    all_scores.append(scores[best])
+                except:
+                    pass
+        return self.pretty_print(all_scores)
---
+++ fatf/interpretability/testing_explainer.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 11:52:13 2018
+
+@author: rp13102
+"""
+import numpy as np
+from cf_explainer import Explainer
+from sklearn.linear_model import LogisticRegression
+from supp_explainer import create_dataset
+
+class CostObject(object):
+    def __init__(self, distance_funcs):
+        self.distance_funcs = distance_funcs
+        
+    """
+    Computes the distance between two instances, based on the distance functions
+    provided by the user.
+    """
+    def dist(self, v0, v1):
+        dist = 0
+        features = v0.dtype.names
+        for feature in features:
+            dist += distance_funcs[feature](v0[feature], v1[feature])
+        return dist
+
+def remove_field(dataset, field):
+    field_names = list(dataset.dtype.names)
+    if field in field_names:
+        field_names.remove(field)
+    return dataset[field_names]
+
+dataset, treatments, distance_funcs = create_dataset()
+targets = dataset['Target']
+dataset = remove_field(dataset, 'Target')
+
+model = LogisticRegression(solver = 'lbfgs')
+
+newx = dataset.copy(order='K')
+newx = np.array(newx.tolist())
+model.fit(newx, targets)
+
+categorical_features = ['Gender']
+
+del distance_funcs['Target']
+x0 = dataset[0]
+print(x0)
+
+feature_ranges = {
+                    'Age': (x0['Age']-20, x0['Age'] + 10),
+                  'Gender': np.array([x0['Gender']])
+                  }
+
+stepsizes = {
+            'Age': 0.25,
+            #'Weight': 0.50
+            }
+
+#cost_calc = CostObject(distance_funcs)
+
+mdl = Explainer(model, 
+                categorical_features, 
+                dataset,
+                monotone = False,
+                stepsizes = stepsizes,
+                feature_ranges=feature_ranges,
+                max_comb = 2,
+                dist_funcs = distance_funcs
+                )
+
+target_class = 0
+c=mdl.generate_counterfactual(x0,
+                            target_class
+                            )
+
+

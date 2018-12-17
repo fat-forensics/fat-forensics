fa752ba538c60914606dfb8bae491dd02c96fceb
RafaelPo <rp13102@bristol.ac.uk>
2018-12-17 17:20:36 +0000

    categorical tests

--- fatf/utils/data/tests/test_augmentation.py
+++ fatf/utils/data/tests/test_augmentation.py
@@ -55,19 +55,43 @@ def test_sample(input_dataset, input_y, test_instance, test_y, expected_output):
 input_t = np.vstack((input_y, 1-input_y))
 test_t_0 = input_t[0]
 test_instance_2cl_0 = input_dataset_0[0]
-expected_output_0 = (np.array([[26, 74]]), np.array([[0.67033678, 0.32966322]]))
+test_instance_2cl_1 = input_dataset_1[0]
+
+expected_output_0 = (np.array([[27, 74]]), np.array([[0.6492304718478048, 0.35076952815219514]]))
+expected_output_1 = (np.array([(27, 74)], dtype=[('Age', '<i4'), ('Weight', '<i4')]), np.array([[0.6492304718478048, 0.35076952815219514]]))
 @pytest.mark.parametrize("input_dataset, input_t, test_instance, test_y, expected_output",
-                         [(input_dataset_0, input_t, test_instance_2cl_0, test_t_0, expected_output_0)])
+                         [(input_dataset_0, input_t, test_instance_2cl_0, test_t_0, expected_output_0),
+                          (input_dataset_1, input_t, test_instance_2cl_1, test_t_0, expected_output_1)])
 def test_sample_2classes(input_dataset, input_t, test_instance, test_y, expected_output):
     mdl = Mixup(input_dataset,
                 input_t)
-    np.random.seed(99)
+    np.random.seed(53)
     output = mdl.sample(test_instance,
                         test_y,
                         1)
-    print(output)
     assert np.all(output[0] == expected_output[0])
     assert np.all(output[1] == expected_output[1])
+  
+input_t = np.vstack((input_y, 1-input_y))
+test_t_0 = input_t[0]
+input_dataset_cat = np.array([(74, 'a'), ( 3, 'b')],
+      dtype=[('Age', '<i4'), ('work', '<U2')])
+
+test_instance_cat_0 = input_dataset_cat[0]
+expected_output_0 = (np.array([(20, 'a')], dtype=[('Age', '<i4'), ('work', '<U2')]), np.array([[0.74975274, 0.25024726]]))
+@pytest.mark.parametrize("input_dataset, input_t, test_instance, test_y, expected_output",
+                         [(input_dataset_cat, input_t, test_instance_cat_0, test_t_0, expected_output_0)])
+def test_sample_categorical(input_dataset, input_t, test_instance, test_y, expected_output):
+    mdl = Mixup(input_dataset,
+                input_t)
+    np.random.seed(7)
+    output = mdl.sample(test_instance,
+                        test_y,
+                        1)
+    assert np.all(output[0] == expected_output[0])
+    assert float(format(output[1][0][0], '.8f')) == expected_output[1][0][0]
+    assert float(format(output[1][0][1], '.8f')) == expected_output[1][0][1]
+    
     
 @pytest.mark.parametrize("input_dataset, input_y",
                          [(input_dataset_0, input_y)])   

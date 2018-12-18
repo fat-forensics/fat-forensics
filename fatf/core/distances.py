a6a9c95467540ed7547784806edbea970d92a50f
Alex Hepburn <ah13558@bristol.ac.uk>
2018-12-18 21:38:25 +0000

    Moved files and deleted old models.py file

--- fatf/core/distances.py
+++
@@ -1,6 +0,0 @@
-"""
-The :mod:`fatf.core.distance` provides some basic distance functions.
-"""
-
-# Author: Alex Hepburn <ah13558@bristol.ac.uk>
-# License: BSD Clause 3
--- fatf/core/lca.py
+++
@@ -1,6 +0,0 @@
-"""
-The :mod:`fatf.core.lca` provides some basic lowest common answer functions.
-"""
-
-# Author: Alex Hepburn <ah13558@bristol.ac.uk>
-# License: BSD Clause 3

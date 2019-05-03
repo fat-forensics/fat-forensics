3065537fbad5534fcb3a14fa11f78f9ff67c385d
Alex Hepburn <ah13558@bristol.ac.uk>
2019-05-03 16:17:51 +0100

    Added abstract class for augmentors and started NormalSampling class

--- fatf/utils/data/augmentation.py
+++ fatf/utils/data/augmentation.py
@@ -1,5 +1,291 @@
-import collections
+"""
+Data augmentation methods
+"""
+# Author: Kacper Sokol <k.sokol@bristol.ac.uk>
+#         Alex Hepburn <ah13558@bristol.ac.uk>
+# License: new BSD
 
+import collections
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+import warnings
+
+import numpy as np
+
+import fatf.utils.array.tools as fuat
+import fatf.utils.array.validation as fuav
+
+from fatf.exceptions import IncorrectShapeError
+
+__all__ = ['NormalSampling']
+
+
+class Augmentation(metaclass=ABCMeta):
+    """
+    Super class for data augmentation.
+
+    A super class that all augmentation classes should inherit from. Contains
+    abstract method for `__init__` and `sample`. Also contains methods for
+    validating the input dataset and indices as well as a separate method for
+    validating the inputs for `sample` function. `_validate_sample_input`
+    should be called from all child classes inside the `sample` function.
+
+    Parameters
+    ----------
+    dataset : numpy.ndarray
+        A 2-dimensional numerical numpy array with a dataset to be used.
+    categorical_indices : numpy.ndarray
+        An array of indices that should be treat as categorical variables.
+
+    Warns
+    -----
+    UserWarning
+        If index of a string type column is not in `categorical_indices` it
+        it will be added.
+    
+    Raises
+    ------
+    IncorrectShapeError
+        The input data is not a 2-dimensional array. The categorical indices
+        list/array (``categorical_features``) is not 1-dimensional.
+    TypeError
+        `categorical_indices` is not eiher a numpy.ndarray or None. `dataset`
+        is not a numpy.ndarray, or `categorical_indices` is None so will be
+        inferred by looking at the data type.
+    
+    Attributes
+    ----------
+    dataset : numpy.ndarray
+        A 2-dimensional numerical numpy array with a dataset to be used.
+    categorical_indices : numpy.ndarray
+        An array of indices that should be treat as categorical variables.
+    """
+    @abstractmethod
+    def __init__(self,
+                 dataset: np.ndarray,
+                 categorical_indices: np.ndarray = None) -> None:
+        """
+        Constructor for Augmentation super class.
+        """
+        assert self._validate_input(dataset, categorical_indices), \
+            'Input is invalid'
+        self.dataset = dataset
+
+        # If string based columns found, add them to categorical_indices.
+        numerical_indices, non_numerical_indices = \
+            fuat.indices_by_type(dataset)
+
+        if categorical_indices is None:
+            if non_numerical_indices.size > 0:
+                categorical_indices = non_numerical_indices
+                msg = ('No categorical_indcies were provided. The categorical '
+                    'columns will be inferred by looking at the type of values '
+                    'in the dataset.')
+                warnings.warn(msg, UserWarning)
+            else:
+                categorical_indices = np.array([])
+        else:
+            if not set(non_numerical_indices).issubset(set(categorical_indices)):
+                msg = ('String based indices were found in dataset but not '
+                       'given as categorical_indices. String based columns '
+                       'will automatically be treated as categorical columns.')
+                warnings.warn(msg, UserWarning)
+                categorical_indices = np.unique(
+                    np.concatenate([categorical_indices, 
+                                    non_numerical_indices]))
+        self.num_features = numerical_indices.shape[0] + \
+            non_numerical_indices.shape[0]
+        self.categorical_indices = categorical_indices
+        # Find indices that aren't in self.categorical_indices
+        if fuav.is_structured_array(self.dataset):
+            indices = np.array(self.dataset.dtype.names)
+        else:
+            indices = np.arange(0, self.dataset.shape[1], 1)
+        self.non_categorical_indices = np.setdiff1d(indices,
+                                                    self.categorical_indices)
+
+
+    def _validate_input(self,
+                        dataset: np.ndarray,
+                        categorical_indices: np.ndarray = None) -> bool:
+        """
+        Validates input array and categorical_indices.
+
+        Parameters
+        ----------
+        dataset : numpy.ndarray
+            A 2-dimensional numerical numpy array with a dataset to be used.
+        categorical_indices : numpy.ndarray
+            An array of indices that should be treat as categorical variables.
+        
+        Raises
+        ------
+        IncorrectShapeError
+            The input data is not a 2-dimensional array. The categorical indices
+            list/array (``categorical_features``) is not 1-dimensional.
+        TypeError
+            `categorical_indices` is not eiher a numpy.ndarray or None. `dataset`
+            is not a numpy.ndarray.
+        
+        Returns
+        -------
+        is_input_ok : boolean
+            If input is valid.
+        """
+        is_input_ok = False
+
+        if not isinstance(dataset, np.ndarray):
+            raise TypeError('dataset must be a numpy.ndarray.')
+
+        if (categorical_indices is not None and not 
+                isinstance(categorical_indices, np.ndarray)):
+            raise TypeError('categorical_indices must be a numpy.ndarray or '
+                            'None')
+        
+        if not fuav.is_2d_array(dataset):
+            raise IncorrectShapeError('The input dataset must be a '
+                                      '2-dimensional array.')
+        
+        if categorical_indices is not None:
+            invalid_indices = fuat.get_invalid_indices(dataset, 
+                                                    categorical_indices)
+            if invalid_indices.size > 0:
+                raise IndexError('Indices {} are not valid for input '
+                                 'dataset.'.format(invalid_indices))
+
+        is_input_ok = True
+    
+        return is_input_ok
+
+
+    def _validate_sample_input(self,
+                               data_row: Optional[np.ndarray] = None,
+                               num_samples: int = 10):
+        """
+        Validates sample input for all `sample` functions.
+        
+        This function checks the validaty of `data_row` with respect to the
+        dataset provided in the constructor for the augmentor.
+
+        Raises
+        ------
+        ValueError
+
+        Returns
+        -------
+        is_input_ok : boolean
+            If input is valid.
+        """
+        is_input_ok = False
+        if (not isinstance(data_row, np.ndarray) and not
+                isinstance(data_row, np.void)):
+            raise TypeError('data_row must be numpy.ndarray.')
+
+        if not isinstance(num_samples, int):
+            raise TypeError('num_samples must be an integer.')
+
+        if num_samples < 1:
+            raise ValueError('num_samples must be an integer greater than 0.')
+
+        if not fuav.is_1d_like(data_row):
+            raise IncorrectShapeError('data_row must be a 1-dimensional '
+                                      'array.')
+
+        if not fuav.are_similar_dtype_arrays(
+                self.dataset, np.array(data_row, dtype=data_row.dtype),
+                strict_comparison=True):
+            raise ValueError('data_row provided is not of the same dtype as '
+                             'the dataset used to initialise this class. '
+                             'Please ensure that the dataset and data_row '
+                             'dtypes are identical.')
+
+        # If structured and different number of features, would be caught
+        # in the previous if statement.
+        if not fuav.is_structured_array(self.dataset):
+            if data_row.shape[0] != self.dataset.shape[1]:
+                raise ValueError('data_row must contain the same number of '
+                                'features as the dataset used in the class '
+                                'constructor.')
+
+        is_input_ok = True
+
+        return is_input_ok
+
+
+    @abstractmethod
+    def sample(self,
+               data_row: Optional[np.ndarray] = None,
+               num_samples: Optional[int] = 10) -> np.ndarray:
+        """
+        Abstract method that must be implemented for each child class.
+
+        Must sample around data_row or if data_row is not given then should
+        generate samples from the whole dataset.
+        """
+        pass
+
+
+class NormalSampling(Augmentation):
+    """
+    Class that samples using a normal distribution (0, 1).
+
+    For additional parameteres, attributes, warnings and exceptions raised
+    please see the  documentation of the :func`fatf.utils.data.Augmentation`
+    class.
+
+    Attributes
+    ----------
+    non_categorical_sampling_values : dictionary[Union[str, int], Tuple[float, float, float]]
+        Dictionary mapping non-categorical feature indices to tuples containing
+        (mean, variance, std) for each non-categorical feature.
+    categorical_sampling_values : dictionary[Union[str, int], Tuple[List[Union[str, int]], np.ndarray]]
+        Dcitionary mapping categorical feature indices to tuples containing
+        (list of values, frequencies of values) for each categorical feature.
+    """
+    def __init__(self,
+                 dataset: np.ndarray,
+                 categorical_indices: np.ndarray = None) -> None:
+        super(NormalSampling, self).__init__(dataset, categorical_indices)
+        numerical_features = \
+            fuat.as_unstructured(self.dataset[self.non_categorical_indices])
+
+        non_categorical_sampling_values = dict()
+        # In case numerical_features is empty array
+        if numerical_features.size > 0:
+            features_mean = np.mean(numerical_features,axis=1)
+            features_var = np.var(numerical_features, axis=1)
+            features_std = np.sqrt(features_var)
+            for cf, mean, var, std in zip(
+                    self.non_categorical_indices, features_mean, features_var, 
+                    features_std):
+                non_categorical_sampling_values[cf] = (mean, var, std)
+        categorical__sampling_values = dict()
+        for cf in self.categorical_indices:
+            feature_vector = self.dataset[cf]
+            feature_counter = collections.Counter(feature_vector)
+            values = list(feature_counter.keys())
+            frequencies = np.array(list(feature_counter.values()),
+                                   dtype=np.float)
+            frequencies /= np.sum(frequencies)
+            categorical__sampling_values[cf] = (values, frequencies)
+
+        self.non_categorical_sampling_values = non_categorical_sampling_values
+        self.categorical_sampling_values = categorical__sampling_values
+
+
+
+    def sample(self,
+               data_row: Optional[np.ndarray] = None,
+               num_samples: Optional[int] = 10) -> np.ndarray:
+        """
+        Samles from normal around `data_row` or around mean.
+
+        If `data_row` is None, then samples will be generated around feature
+        means using `self.feature_means` attribute.
+        """
+        self._validate_sample_input(data_row, num_samples)
+        pass
+'''
 def lime(num_samples,
          data_set,
          data_row=None):
@@ -80,3 +366,4 @@ def lime_data(generated_data, original_data):
         binary_column[0] = 1
         data[:, column] = binary_column
     pass
+'''
---
+++ fatf/utils/data/tests/test_augmentation.py
@@ -0,0 +1,240 @@
+"""
+Functions for testing data augmentation classes.
+"""
+# Author: Alex Hepburn <ah13558@bristol.ac.uk>
+#         Kacper Sokol <k.sokol@bristol.ac.uk>
+# License: new BSD
+
+import pytest
+
+import numpy as np
+
+import fatf
+from fatf.exceptions import IncorrectShapeError
+import fatf.utils.data.augmentation as fuda
+from fatf.utils.testing.arrays import (BASE_NP_ARRAY, BASE_STRUCTURED_ARRAY,
+                                       NOT_BASE_NP_ARRAY)
+
+ONE_D_ARRAY = np.array([0, 4, 3, 0])
+
+NUMERICAL_NP_ARRAY = np.array([
+    [0, 0, 0.08, 0.69],
+    [1, 0, 0.03, 0.29],
+    [0, 1, 0.99, 0.82],
+    [2, 1, 0.73, 0.48],
+    [1, 0, 0.36, 0.89],
+    [0, 1, 0.07, 0.21]])
+NUMERICAL_STRUCT_ARRAY = np.array(
+    [(0, 0, 0.08, 0.69),
+     (1, 0, 0.03, 0.29),
+     (0, 1, 0.99, 0.82),
+     (2, 1, 0.73, 0.48),
+     (1, 0, 0.36, 0.89),
+     (0, 1, 0.07, 0.21)],
+    dtype=[('a', 'i'), ('b', 'i'), ('c', 'f'), ('d', 'f')])
+CATEGORICAL_NP_ARRAY = np.array([
+    ['a', 'b', 'c'],
+    ['a', 'f', 'g'],
+    ['b', 'c', 'c']])
+CATEGORICAL_STRUCT_ARRAY = np.array(
+    [('a', 'b', 'c'),
+     ('a', 'f', 'g'),
+     ('b', 'c', 'c')],
+    dtype=[('a', 'U1'), ('b', 'U1'), ('c', 'U1')])
+MIXED_ARRAY = np.array(
+    [(0, 'a', 0.08, 'a'),
+     (0, 'f', 0.03, 'bb'),
+     (1, 'c', 0.99, 'aa'),
+     (1, 'a', 0.73, 'a'),
+     (0, 'c', 0.36, 'b'),
+     (1, 'f', 0.07, 'bb')],
+    dtype=[('a', 'i'), ('b', 'U1'), ('c', 'f'), ('d', 'U2')])
+
+
+class BaseAugmentor(fuda.Augmentation):
+    """
+    Dummy class to test :func`fatf.utils.data.augmentation.Augmentation.
+    _validate_input` and :func`fatf.utils.data.augmentation.Augmentation.
+    _validate_sample_input`.
+
+    """
+    def __init__(self, dataset, categorical_indices=None):
+        super(BaseAugmentor, self).__init__(dataset, categorical_indices)
+    
+    def sample(self, data_row=None, num_samples=10):
+        self._validate_sample_input(data_row, num_samples)
+
+
+class BrokenAugmentor(fuda.Augmentation):
+    """
+    Class with no `sample` function defined.
+    """
+    def __init__(self, dataset, categorical_indices=None):
+        super(BaseAugmentor, self).__init__(dataset, categorical_indices)
+
+
+def test_Augmentation():
+    """
+    tests :class`fatf.utils.data.augmentation.Augmentation`
+    """
+    msg = ('Can\'t instantiate abstract class BrokenAugmentor with abstract '
+           'methods sample')
+    with pytest.raises(TypeError) as exin:
+        augmentor = BrokenAugmentor(NUMERICAL_NP_ARRAY)
+    assert str(exin.value) == msg
+
+    msg = ('dataset must be a numpy.ndarray.')
+    with pytest.raises(TypeError) as exin:
+        augmentor = BaseAugmentor(0)
+    assert str(exin.value) == msg
+
+    msg = ('categorical_indices must be a numpy.ndarray or None')
+    with pytest.raises(TypeError) as exin:
+        augmentor = BaseAugmentor(NUMERICAL_NP_ARRAY, categorical_indices=0)
+    assert str(exin.value) == msg
+
+    msg = ('The input dataset must be a 2-dimensional array.')
+    with pytest.raises(IncorrectShapeError) as exin:
+        augmentor = BaseAugmentor(ONE_D_ARRAY, np.array([0]))
+    assert str(exin.value) == msg
+
+    msg = ('Indices {} are not valid for input dataset.')
+    with pytest.raises(IndexError) as exin:
+        augmentor = BaseAugmentor(NUMERICAL_NP_ARRAY, np.array([10]))
+    assert str(exin.value) == msg.format(np.array([10]))
+    with pytest.raises(IndexError) as exin:
+        augmentor = BaseAugmentor(NUMERICAL_NP_ARRAY, np.array(['a']))
+    assert str(exin.value) == msg.format(np.array(['a']))
+    with pytest.raises(IndexError) as exin:
+        augmentor = BaseAugmentor(NUMERICAL_STRUCT_ARRAY, np.array(['l']))
+    assert str(exin.value) == msg.format(np.array(['l']))
+
+    msg = ('No categorical_indcies were provided. The categorical columns '
+           'will be inferred by looking at the type of values in the dataset.')
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(CATEGORICAL_NP_ARRAY)
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, np.array([0, 1, 2]))
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(CATEGORICAL_STRUCT_ARRAY)
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, 
+                          np.array(['a', 'b', 'c']))
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(MIXED_ARRAY)
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, np.array(['b', 'd']))
+
+    msg = ('String based indices were found in dataset but not given as '
+           'categorical_indices. String based columns will automatically be '
+           'treated as categorical columns.')
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(CATEGORICAL_NP_ARRAY, np.array([0]))
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, np.array([0, 1, 2]))
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(CATEGORICAL_STRUCT_ARRAY, np.array(['a']))
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, 
+                          np.array(['a', 'b', 'c']))
+    with pytest.warns(UserWarning) as warning:
+        augmentor = BaseAugmentor(MIXED_ARRAY, np.array(['b']))
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices, np.array(['b', 'd']))
+
+    # Validate sample input rows
+    augmentor = BaseAugmentor(NUMERICAL_NP_ARRAY)
+    msg = ('data_row must be numpy.ndarray.')
+    with pytest.raises(TypeError) as exin:
+        sample = augmentor.sample(1)
+    assert str(exin.value) == msg
+
+    msg = ('num_samples must be an integer.')
+    with pytest.raises(TypeError) as exin:
+        sample = augmentor.sample(np.array([]), 'a')
+    assert str(exin.value) == msg
+
+    msg = ('num_samples must be an integer greater than 0.')
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(np.array([]), -1)
+    assert str(exin.value) == msg
+
+    msg = ('data_row provided is not of the same dtype as the dataset used to '
+           'initialise this class. Please ensure that the dataset and data_row '
+           'dtypes are identical.')
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(np.array(['a', 'b', 'c', 'd']))
+    assert str(exin.value) == msg
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(MIXED_ARRAY[0])
+    assert str(exin.value) == msg
+    augmentor = BaseAugmentor(CATEGORICAL_STRUCT_ARRAY, 
+                              np.array(['a', 'b', 'c']))
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(np.array([0.1]))
+    assert str(exin.value) == msg
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(MIXED_ARRAY[0][['a', 'b']])
+    assert str(exin.value) == msg
+
+    augmentor = BaseAugmentor(NUMERICAL_NP_ARRAY)
+    msg = ('data_row must contain the same number of features as the dataset '
+           'used in the class constructor.')
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(np.array([0.1]))
+    assert str(exin.value) == msg
+    augmentor = BaseAugmentor(CATEGORICAL_NP_ARRAY, np.array([0, 1, 2]))
+    with pytest.raises(ValueError) as exin:
+        sample = augmentor.sample(np.array(['a']))
+    assert str(exin.value) == msg
+
+    msg = ('data_row must be a 1-dimensional array.')
+    with pytest.raises(IncorrectShapeError) as exin:
+        sample = augmentor.sample(NUMERICAL_NP_ARRAY)
+    assert str(exin.value) == msg
+    augmentor = BaseAugmentor(NUMERICAL_STRUCT_ARRAY)
+    with pytest.raises(IncorrectShapeError) as exin:
+        sample = augmentor.sample(NUMERICAL_STRUCT_ARRAY)
+    assert str(exin.value) == msg
+
+
+def test_NormalSampling():
+    """
+    tests :func`fatf.utils.data.augmentation.NormalSampling`
+    """
+    fatf.setup_random_seed()
+
+    # Test class inheritence and calcuating non_categorical_indices
+    # and categorical_indices
+    augmentor = fuda.NormalSampling(NUMERICAL_NP_ARRAY, np.array([0]))
+    assert augmentor.__class__.__bases__[0].__name__ == 'Augmentation'
+    assert np.array_equal(augmentor.categorical_indices, np.array([0]))
+    assert np.array_equal(augmentor.non_categorical_indices,
+                          np.array([1, 2, 3]))
+
+    augmentor = fuda.NormalSampling(NUMERICAL_NP_ARRAY)
+    assert np.array_equal(augmentor.categorical_indices, np.array([]))
+    assert np.array_equal(augmentor.non_categorical_indices,
+                          np.array([0, 1, 2, 3]))
+
+    augmentor = fuda.NormalSampling(NUMERICAL_STRUCT_ARRAY, np.array(['a']))
+    assert np.array_equal(augmentor.categorical_indices, np.array(['a']))
+    assert np.array_equal(augmentor.non_categorical_indices,
+                          np.array(['b', 'c', 'd']))
+
+    msg = ('No categorical_indcies were provided. The categorical columns '
+           'will be inferred by looking at the type of values in the dataset.')
+    with pytest.warns(UserWarning) as warning:
+        augmentor = fuda.NormalSampling(CATEGORICAL_NP_ARRAY)
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert np.array_equal(augmentor.categorical_indices,
+                          np.array([0, 1, 2]))
+    assert np.array_equal(augmentor.non_categorical_indices, np.array([]))
+    assert False

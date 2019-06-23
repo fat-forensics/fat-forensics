bec148c1a3c83ec56669f8cc6bab84baff4fe837
Rafael Poyiadzi <rp13102@bristol.ac.uk>
2019-06-23 23:18:19 +0200

    cleaned the code. working for numerical data. renamed the sklearn folder

--- fatf/transparency/gradient_explainer.py
+++ fatf/transparency/gradient_explainer.py
@@ -7,7 +7,7 @@ functions relevant to performing counterfactual explanations.
 # License: BSD 3 clause
 
 import numpy as np
-from typing import Optional, Union, Callable, List, Dict
+from typing import Optional, Callable, List, Dict
 import matplotlib.pyplot as plt
 from itertools import cycle
 
@@ -17,19 +17,19 @@ class Explainer2(object):
     """
     def __init__(self,
                  model: Callable,
-                 reg: Optional[Union[float, int]] = None,
                  threshold: Optional[float] = None,
                  boundaries: Optional[Dict] = None,
-                 cost_func: Optional[Callable] = None):
+                 ):
 
-        self.step = 0.00001
-        self.cycle = True
+        # for estimating the gradient
+        self.step = 1e-7
+        
+        # for gradient descent
+        self.step_size = 1e-2
+        
         self.last_diff = 10000
-        if reg is not None:
-            self.reg = [reg]
-        else:
-            self.reg = [5, 10, 20]
 
+        self.n_classes = model.classes_.shape[0]
         self.model = model
         if not threshold:
             self.threshold = 0.05
@@ -39,18 +39,13 @@ class Explainer2(object):
         self.counter = 0
         self.stagnation_bound = 10
 
-        self.own_func = False
-        if cost_func is not None:
-            self.cost_func = cost_func
-            self.own_func = True
-
     def __estimate_gradient(self,
                             instance: np.array):
         idx = self.counter
         instance[idx] += self.step
-        eval_up = self.model.predict_proba(instance.reshape(1, -1))
+        eval_up = self.model.predict_proba(instance.reshape(1, -1))[0][self.target_class]
         instance[idx] -= 2 * self.step
-        eval_down = self.model.predict_proba(instance.reshape(1, -1))
+        eval_down = self.model.predict_proba(instance.reshape(1, -1))[0][self.target_class]
         instance[idx] += self.step
         grad_fx = (eval_up - eval_down) / (2 * self.step)
 
@@ -58,50 +53,40 @@ class Explainer2(object):
 
     def __eval_condition(self,
                          test_prediction: np.array) -> bool:
-        diff = np.linalg.norm(self.target_prediction - test_prediction)
-# =============================================================================
-#         if diff > self.last_diff + 0.2:
-#             return True
-# =============================================================================
+        diff = np.abs(1 - test_prediction)
         self.last_diff = diff
         return diff < self.threshold
 
     def __optimise(self,
                    instance: np.array,
-                   reg: int) -> np.array:
+                   ) -> np.array:
         self.st = 0
         satisfied = self.__eval_condition(self.current_prediction)
         stagnated = 0
         while not satisfied:
-            if self.cycle:
-                self.counter += 1
-                self.counter = self.counter%self.n_ftrs
-                if self.counter in self.nottochange:
-                    continue
-            else:
-                self.counter = np.random.choice(self.tosamplefrom)
+            self.counter += 1
+            self.counter = self.counter%self.n_ftrs
+            if self.counter in self.nottochange:
+                continue
             self.st += 1
             grad_fx = self.__estimate_gradient(instance)
-            part0 = np.linalg.norm(grad_fx)**2
-            part1a = self.model.predict_proba(instance.reshape(1, -1)).reshape(-1, 1)
-            part1b = self.target_prediction - part1a
-            part1 = np.dot(part1b.T, grad_fx)
-            delta = part1 / (part0 + reg)
+            grad_fx *= self.step_size
             if self.boundaries is not None:
-                newx_i = instance[self.counter] + 2 * delta
+                newx_i = instance[self.counter] + grad_fx
                 if (newx_i >= self.boundaries[self.counter]['min'] and
                     newx_i <= self.boundaries[self.counter]['max']):
                     instance[self.counter] = newx_i
                     stagnated = 0
-                    part1a = self.model.predict_proba(instance.reshape(1, -1)).reshape(-1, 1)
-                    satisfied = self.__eval_condition(part1a)
+                    pred = self.model.predict_proba(instance.reshape(1, -1))[0][self.target_class]
+                    satisfied = self.__eval_condition(pred)
                 else:
                     stagnated += 1
                     if stagnated == self.stagnation_bound:
                         return instance
             else:
-                instance[self.counter] += 2 * delta
-                satisfied = self.__eval_condition(part1a)
+                instance[self.counter] += grad_fx
+                pred = self.model.predict_proba(instance.reshape(1, -1))[0][self.target_class]
+                satisfied = self.__eval_condition(pred)
             n_steps = 1000
             colors=cycle(list((plt.cm.rainbow(np.linspace(0,1,n_steps)))))
 
@@ -110,15 +95,12 @@ class Explainer2(object):
 
     def explain(self,
                 subject_instance: np.array,
-                target_prediction: np.array,
+                target_class: int,
                 nottochange: Optional[List[int]] = None):
         self.subject_instance = subject_instance
-        self.target_prediction = target_prediction.reshape(-1, 1)
-        self.n_classes = self.target_prediction.shape[0]
+        self.target_class = target_class
         self.n_ftrs = len(self.subject_instance)
-        self.current_prediction = self.model.predict_proba(
-                                    self.subject_instance.reshape(1, -1))
-
+        self.current_prediction = self.model.predict_proba(self.subject_instance.reshape(1, -1))[0][self.target_class]
         if not nottochange:
             self.nottochange = []
             self.tosamplefrom = self.n_ftrs
@@ -129,19 +111,4 @@ class Explainer2(object):
                 self.tosamplefrom.pop(self.tosamplefrom.index(item))
 
         instance = self.subject_instance.copy(order='K')
-        previous_modified_x = 0
-        for reg in self.reg:
-            print(reg)
-            modified_x = self.__optimise(instance, reg)
-
-            pred = self.model.predict(modified_x.reshape(1, -1))
-            print(modified_x, pred)
-            if pred != np.argmax(self.target_prediction):
-                return previous_modified_x
-            else:
-                previous_modified_x = modified_x
-            print(previous_modified_x)
-            print('\n')
-        return modified_x
-
-
+        return self.__optimise(instance)
--- fatf/transparency/testing_explainer2.py
+++ fatf/transparency/testing_explainer2.py
@@ -7,14 +7,18 @@ Created on Wed Dec  5 11:52:13 2018
 import numpy as np
 from gradient_explainer import Explainer2
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import make_classification
+from sklearn.neural_network import MLPClassifier as NN
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
 n_ftrs = 2
-X, y = make_classification(n_features=n_ftrs, n_redundant=0, n_informative=1,
+X, y = datasets.make_classification(n_features=n_ftrs, n_redundant=0, n_informative=1,
                              n_clusters_per_class=1)
+
+X, y = datasets.make_moons(100, noise=.10)
 X = X - np.mean(X, axis = 0)
 X = X / np.std(X, axis = 0)
-mdl = LogisticRegression(solver = 'lbfgs')
+mdl = NN(max_iter=int(1e5))
 mdl.fit(X, y)
 pr_proba = mdl.predict_proba(X)
 pr = mdl.predict(X)
@@ -25,22 +29,26 @@ boundaries = {}
 for i in range(n_ftrs):
     boundaries[i] = {'min': min_values[i],
                       'max': max_values[i]}
+    
+boundaries[1]['max'] = 1.5
 
 if pr[0] == 0:
-    target = np.array([0, 1])
+    target = 1
 else:
-    target = np.array([1, 0])
+    target = 0
 
 plt.figure()
 plt.scatter(X[:, 0], X[:, 1], c=pr_proba[:, 1])
 plt.scatter(X[0, 0], X[0, 1], marker='x')
 #plt.colorbar()
 xpl = Explainer2(mdl,
-                 reg = 5,
+                 boundaries=boundaries
                  )
-newx = xpl.explain(X[0, :],
-                   target,
-                   nottochange=[1])
+newx = xpl.explain(
+            X[0, :],
+            target,
+            #nottochange=[1]
+            )
 
 if newx is not 0:
     print(mdl.predict_proba(newx.reshape(1, -1)), target)

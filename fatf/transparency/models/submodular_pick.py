ea8e0427f991dc898e34b8ec1edd4548fce4a760
Kacper Sokol <ks1591@my.bristol.ac.uk>
2022-04-08 21:17:08 +1000

    Submodular pick testing and formatting

--- fatf/transparency/models/submodular_pick.py
+++ fatf/transparency/models/submodular_pick.py
@@ -1,205 +1,230 @@
 """
-Implements submodular pick algorithm.
+The :mod:`fatf.transparency.models.submodular_pick` module implements the
+submodular pick algorithm proposed by [RIBEIRO2016WHY]_.
+
+.. versionadded:: 0.1.1
+
+.. [RIBEIRO2016WHY] Ribeiro, M.T., Singh, S. and Guestrin, C., 2016,
+   August. Why should I trust you?: Explaining the predictions of any
+   classifier. In Proceedings of the 22nd ACM SIGKDD international
+   conference on knowledge discovery and data mining (pp. 1135-1144). ACM.
 """
 # Author: Alex Hepburn <ah13558@bristol.ac.uk>
+#         Kacper Sokol <k.sokol@bristol.ac.uk>
 # License: new BSD
 
-from typing import Optional, Union, List, Dict
+from numbers import Number
+from typing import Callable, Dict, List, Union
+
+import logging
 import warnings
 
 import numpy as np
 
+from fatf.exceptions import IncorrectShapeError
+
 import fatf.utils.array.validation as fuav
 import fatf.utils.validation as fuv
 
-from fatf.exceptions import IncompatibleExplainerError, IncorrectShapeError
+__all__ = ['submodular_pick']
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 Index = Union[int, str]
 
-__all__ = ['submodular_pick']
 
-
-def _input_is_valid(dataset: np.ndarray,
-                    explainer: object,
-                    sample_size: Optional[int],
-                    num_explanations: Optional[int]) -> bool:
+def _validate_input(dataset: np.ndarray, explain_instance: Callable,
+                    sample_size: int, explanations_number: int) -> bool:
     """
     Validates input for submodular pick.
 
-    For the input parameter description, warnings and exceptions please see the
-    documentation of the :func`fatf.transparency.models.submodular_pick`
+    For the input parameters description, warnings and exceptions please see
+    the documentation of the :func:`fatf.transparency.models.submodular_pick`
     function.
 
     Returns
     -------
-    is_input_ok : boolean
+    is_valid : boolean
         ``True`` if the input is valid, ``False`` otherwise.
     """
-    is_input_ok = True
+    is_valid = False
 
     if not fuav.is_2d_array(dataset):
-        raise IncorrectShapeError('The input dataset must be a 2-dimensional '
-                                  'array.')
-
+        raise IncorrectShapeError(
+            'The input data set must be a 2-dimensional array.')
     if not fuav.is_base_array(dataset):
-        raise ValueError('The input dataset must only contain base types '
-                         '(textual and numerical).')
+        raise ValueError('The input data set must only contain base types '
+                         '(strings and numbers).')
 
-    if sample_size is not None and not isinstance(sample_size, int):
-        raise TypeError('sample_size must be an integer or None.')
+    if not isinstance(sample_size, int):
+        raise TypeError('sample_size must be an integer.')
+    if sample_size < 0:
+        raise ValueError('sample_size must be a non-negative integer.')
 
-    if sample_size is not None and sample_size <= 0:
-        raise ValueError('sample_size must be a positive integer or None.')
+    if not isinstance(explanations_number, int):
+        raise TypeError('explanations_number must be an integer.')
+    if explanations_number is not None and explanations_number < 0:
+        raise ValueError('explanations_number must be a non-negative integer.')
 
-    if num_explanations is not None and not isinstance(num_explanations, int):
-        raise TypeError('num_explanations must be an integer or None.')
+    if (sample_size and explanations_number
+            and sample_size < explanations_number):
+        raise ValueError('The number of explanations cannot be larger than '
+                         'the number of samples.')
 
-    if num_explanations is not None and num_explanations < 0:
-        raise ValueError('num_explanations must be a positive integer or '
-                         'None.')
+    if callable(explain_instance):
+        params_n = fuv.get_required_parameters_number(explain_instance)
+        if params_n != 1:
+            raise RuntimeError('The explain_instance callable must accept '
+                               'exactly one required parameter.')
+    else:
+        raise TypeError('The explain_instance should be a Python callable '
+                        '(function or method).')
 
-    if ((sample_size is not None and num_explanations is not None) and
-            (sample_size < num_explanations)):
-        raise ValueError('sample_size must be larger or equal to '
-                         'num_explanations.')
-
-    if not fuv.check_explainer_functionality(explainer, True):
-        raise IncompatibleExplainerError(
-            'explainer object must be method \'explain_instance\' which has '
-            'exactly one required parameter. Other named parameters can be '
-            'passed to the submodular pick method.')
-
-    return is_input_ok
+    is_valid = True
+    return is_valid
 
 
-def submodular_pick(dataset: np.ndarray,
-                    explainer: object,
-                    sample_size: Optional[int] = None,
-                    num_explanations: Optional[int] = 5,
-                    **kwargs) -> List[Dict[Union[int, str], np.float64]]:
+def submodular_pick(
+        dataset: np.ndarray,
+        explain_instance: Callable,
+        sample_size: int = 0,
+        explanations_number: int = 5) -> List[Dict[Union[int, str], Number]]:
     """
-    Performs submodular pick with given explainer.
+    Applies submodular pick to explanations of a given subset of data.
 
-    Chooses the most informative explanations based on the submodular pick
-    algorithm described in the original LIME paper [1]_.
-    Performs a greedy optimisation to maximise the coverage function for
-    explanations of various points in the dataset.
-    The explainer object should contain a method called ``explain_instance``
-    that has exactly one required parameter and returns a dictionary mapping
-    feature names or indices to a float value indicating that features
-    importance. In order to pass additioanl arguments to
-    ``explainer.explain_instnace`` function please use ``**kwargs``.
+    .. versionadded:: 0.1.1
+
+    Chooses the most informative data point explanations using the submodular
+    pick algorithm introduced by [RIBEIRO2016WHY]_.
+    Submodular pick applies a greedy optimisation to maximise the coverage
+    function for explanations of a subset of points taken from the input
+    data set.
+    The explanation function (``explain_instance``) must have exactly one
+    required parameter and return a dictionary mapping feature names or indices
+    to their respective importance.
+
+    References
+    ----------
+    .. [RIBEIRO2016WHY] Ribeiro, M.T., Singh, S. and Guestrin, C., 2016,
+       August. Why should I trust you?: Explaining the predictions of any
+       classifier. In Proceedings of the 22nd ACM SIGKDD international
+       conference on knowledge discovery and data mining (pp. 1135-1144). ACM.
 
     Parameters
     ----------
     dataset : numpy.ndarray
-        A dataset based on which submodule pick will be computed.
-    explainer : object
-        An object used to get explainations for a model around instances. This
-        object must contain a method ``explain_instance`` that returns an
-        explanation around a provided row in the dataset. An explanation
-        should be of type of `Dict[Union[int, str], np.float64]]`,
-        mapping feature names or indices to the corresponding feature
-        importance.
-    sample_size : integer, optional (default=None)
-        Number of samples in the subset of instances used to generate
-        explainations for. If `None`, then explanations will be generated for
-        the whole dataset.
-    num_explanations : integer, optional (default=5)
-        Number of explanations to return. If `None`, then an ordered list of
-        all explanations sampled data are returned.
-    **kwargs : explainer.explain_instance
-        `explain_instance` optional parameters (e.g. number of sampels to
-        generation around instance during explanation)
+        A data set from which to select individual instances to be explained.
+    explain_instance : callable
+        A reference to a function or method that can generate an explanation
+        from an array representing an individual instance.
+        This callable must accept exactly one required parameter and return
+        an explanation around the selected data point -- a dictionary mapping
+        feature names or indices to their importance.
+    sample_size : integer, optional (default=0)
+        The number of (randomly selected) data points for which to generate
+        explanations. If ``0``, explanations for all the data points in the
+        ``dataset`` will be generated.
+    explanations_number : integer, optional (default=5)
+        The number of explanations to return. If ``0``, an ordered list of
+        all explanations generated for the selected data subset are returned.
 
     Warns
     -----
     UserWarning
-        ``sample_size`` is larger than the number of samples in the dataset,
-        and as such the entire dataset will be used.
+        ``sample_size`` is larger than the number of instances (rows) available
+        in the ``dataset``, in which case the entire data set is used.
+        The number of the requested explanations is larger than the number of
+        instances selected to generate explanations -- explanations for all
+        the data points in the sample will be generated.
 
     Raises
     ------
     IncorrectShapeError
-        The input dataset is not a 2-dimensional numpy array.
+        The input data set is not a 2-dimensional numpy array.
     TypeError
-        ``sample_size`` is not None` or integer, ``num_explanations`` is
-        not `None or integer.
+        ``sample_size`` or ``explanations_number`` is not an integer.
+        ``explain_instance`` is not Python callable (function or method).
     ValueError
-        The input dataset must only contain base types (textual and numerical
-        values). ``sample_size`` is not positive integer, ``num_explanations``
-        is not positive integer.
-    IncompatibleExplainerError
-        Explainer object either has no method called ``explain_instance`` or
-        ``explain_instance`` method does not have exactly one required
+        The input data set must only contain base types (strings and numbers).
+        ``sample_size`` or ``explanations_number`` is a negative integer.
+        The number of requested explanations is larger than the number of
+        samples in the data set.
+    RuntimeError
+        The ``explain_instance`` callable does not require exactly one
         parameter.
 
     Returns
     -------
-    submodular_pick_explanations : List[Dictionary[Union[integer, string],
-            numpy.float64]]
-        List of explanations chosen by submodular pick algorithm.
-    submodular_pick_indices : List[integer]
-        List of indices of rows in dataset which were chosen by submodular
-        pick.
-
-    References
-    ----------
-    .. [1] Ribeiro, Marco Tulio, Sameer Singh, and Carlos Guestrin. "Why
-       should i trust you?: Explaining the predictions of any classifier."
-       Proceedings of the 22nd ACM SIGKDD international conference on
-       knowledge discovery and data mining. ACM, 2016.
+    sp_explanations : List[Dictionary[Union[integer, string], Number]]
+        List of explanations chosen by the submodular pick algorithm.
+    sp_indices : List[integer]
+        List of indices for rows in the ``dataset`` chosen (and explained)
+        by the submodular pick algorithm.
     """
-    assert _input_is_valid(dataset, explainer, sample_size,
-                           num_explanations), 'Input must be valid.'
+    # pylint: disable=too-many-locals
+    assert _validate_input(dataset, explain_instance, sample_size,
+                           explanations_number), 'Invalid input.'
 
-    sampled = False
-    if sample_size is None:
-        rows = dataset
+    if sample_size == 0:
+        sample_size = dataset.shape[0]
+        sample_indices = np.arange(sample_size, dtype=int)
     elif sample_size > dataset.shape[0]:
-        msg = ('sample_size is larger than the number of sampels in the '
-               'dataset. The whole dataset will be used.')
-        warnings.warn(msg, UserWarning)
-        rows = dataset
+        warnings.warn(
+            'sample_size is larger than the number of samples in the data '
+            'set. The whole dataset will be used.', UserWarning)
+        sample_size = dataset.shape[0]
+        sample_indices = np.arange(sample_size, dtype=int)
     else:
-        random_indices = np.random.choice(dataset.shape[0], sample_size)
-        rows = dataset[random_indices]
-        sampled = True
+        sample_indices = np.random.choice(
+            dataset.shape[0], sample_size, replace=False)
 
-    if num_explanations is None:
-        num_explanations = rows.shape[0]
+    if explanations_number == 0:
+        explanations_number = sample_size
+    if explanations_number > sample_size:
+        warnings.warn(
+            'The number of explanations cannot be larger than the number of '
+            'instances (rows) in the data set.', UserWarning)
+        explanations_number = sample_size
 
+    # Explain selected rows
     explanations = []
-    for row in rows:
-        explanations.append(explainer.explain_instance(row, **kwargs))
+    for row in dataset[sample_indices]:
+        explanations.append(explain_instance(row))
+    explanations_no = len(explanations)
 
-    feature_set = set().union(*(d.keys() for d in explanations))
-    feature_dict = dict(zip(list(feature_set), list(range(len(feature_set)))))
+    # Get unique feature names and fix index mapping
+    feature_names = set()
+    for exp in explanations:
+        feature_names = feature_names.union(exp.keys())
+    feature_names_no = len(feature_names)
+    feature_map = {id_: i for i, id_ in enumerate(sorted(feature_names))}
 
-    weights = np.zeros((len(explanations), len(feature_set)))
+    # Compute explanation matrix -- explanations X importances -- i.e., weights
+    weights = np.zeros((explanations_no, feature_names_no))
     for i, exp in enumerate(explanations):
-        ind = np.array([feature_dict[key] for key in exp.keys()])
-        val = np.array([exp[key] for key in exp.keys()])
-        weights[i, ind] = val
-    importance = np.sum(np.abs(weights), axis=0)**.5
+        keys = sorted(exp.keys())
+        idx = [feature_map[key] for key in keys]
+        val = [exp[key] for key in keys]
+        weights[i, idx] = val
 
-    remaining_indices = set(range(len(explanations)))
-    submodular_pick_indices = []
-    for _ in range(num_explanations):
-        coverages = []
-        for i in remaining_indices:
-            weights_abs = np.abs(weights)[submodular_pick_indices + [i]]
-            coverage = np.dot((np.sum(weights_abs, axis=0) > 0), importance)
-            coverages.append((i, coverage))
-        best_ind = coverages[np.argmax([k[1] for k in coverages])][0]
-        submodular_pick_indices.append(best_ind)
-        remaining_indices -= {best_ind}
+    importance = np.abs(weights).sum(axis=0)**.5
 
-    submodular_pick_explanations = [explanations[i] for i in
-                                    submodular_pick_indices]
+    indices = set(range(explanations_no))
+    sp_indices = []
+    for _ in range(explanations_number):
+        coverage = []
+        for i in indices:
+            indices_ = sp_indices + [i]
+            global_importance = np.abs(weights)[indices_].sum(axis=0)
+            global_importance_indicator = global_importance > 0
+            coverage_ = np.dot(global_importance_indicator, importance)
+            coverage.append((i, coverage_))
+        coverage = sorted(coverage, key=lambda i: i[1], reverse=True)
+        best_idx = coverage[0][0]
+        sp_indices.append(best_idx)
+        indices -= {best_idx}
 
-    if sampled:
-        submodular_pick_indices = \
-            random_indices[submodular_pick_indices].tolist()
+    sp_explanations = [explanations[i] for i in sp_indices]
+    sp_indices = sample_indices[sp_indices].tolist()
 
-    return submodular_pick_explanations, submodular_pick_indices
+    return sp_explanations, sp_indices
--- fatf/transparency/models/tests/test_submodular_pick.py
+++ fatf/transparency/models/tests/test_submodular_pick.py
@@ -1,25 +1,22 @@
 """
-Tets fatf.transparency.models.submodular_pick.
+Tets the :mod:`fatf.transparency.models.submodular_pick` module.
 """
 # Author: Alex Hepburn <ah13558@bristol.ac.uk>
+#         Kacper Sokol <k.sokol@bristol.ac.uk>
 # License: new BSD
-
-import abc
 
 import pytest
 
 import numpy as np
 
+from fatf.exceptions import IncorrectShapeError
+from fatf.utils.testing.arrays import NOT_BASE_NP_ARRAY
+
 import fatf
+
 import fatf.transparency.models.submodular_pick as ftms
-import fatf.utils.data.datasets as fudd
-from fatf.exceptions import IncompatibleExplainerError, IncorrectShapeError
-from fatf.utils.testing.arrays import (BASE_NP_ARRAY, BASE_STRUCTURED_ARRAY,
-                                       NOT_BASE_NP_ARRAY)
 
-from sklearn.linear_model import LogisticRegression, Ridge
-
-
+# yapf: disable
 NUMERICAL_NP_ARRAY = np.array([
     [0, 0, 0.08, 0.69],
     [1, 0, 0.03, 0.29],
@@ -33,148 +30,150 @@ CATEGORICAL_STRUCT_ARRAY = np.array(
     dtype=[('a', 'U1'), ('b', 'U1'), ('c', 'U1')])
 
 EXPLAINERS = [
-    {'a': 0.1,
-     'b': 0.1,
-     'c': 0.1,
-     'd': 0.1},
-    {'a': 0.1,
-     'b': 0.1},
-    {'e': 0.1,
-     'd': 0.1},
-    {'a': 0.9,
-     'b': 0.9,
-     'c': 0.9,
-     'd': 0.9}]
+    {'a': 0.1, 'b': 0.1, 'c': 0.1, 'd': 0.1},
+    {'a': 0.1, 'b': 0.1},
+    {'e': 0.1, 'd': 0.1},
+    {'a': 0.9, 'b': 0.9, 'c': 0.9, 'd': 0.9}]
+# yapf: enable
 
 
-class DummyExplainer(abc.ABC):
-    @abc.abstractmethod
-    def __init__(self, dataset):
-        self.dataset = dataset
-    
-    def explain_instance(self, instance):
-        ind = np.where((self.dataset == instance).all(axis=1))
-        i = ind[0][0]
-        return self.explainers[i]
-
-class DummyExplainer1(DummyExplainer):
-    def __init__(self, dataset):
-        self.dataset = dataset
-        self.explainers = EXPLAINERS
-
-class DummyExplainer2(DummyExplainer):
-    def __init__(self, dataset):
-        self.dataset = dataset
-        self.explainers = EXPLAINERS[::-1]
+def explain_instance_a(instance):
+    ind = np.where((NUMERICAL_NP_ARRAY == instance).all(axis=1))
+    i = ind[0][0]
+    return EXPLAINERS[i]
 
 
-def test_input_is_valid():
+def explain_instance_b(instance):
+    ind = np.where((NUMERICAL_NP_ARRAY == instance).all(axis=1))
+    i = ind[0][0]
+    return EXPLAINERS[::-1][i]
+
+
+def test_validate_input():
     """
-    Tests :func:`fatf.transparency.models.submodular_pick._is_input_valid`.
+    Tests :func:`fatf.transparency.models.submodular_pick._validate_input`.
     """
-    class FakeExplainer():
-        def __init__(self): pass
-    
-    class WorkingExplainer():
-        def __init__(self): pass
-        def explain_instance(self, x, labels=1): pass
-    
-    fake_explainer = FakeExplainer()
-    working_explainer = WorkingExplainer()
+    explain_instance = lambda x: x + 1  # noqa: E731
 
-    msg = ('The input dataset must be a 2-dimensional array.')
+    msg = 'The input data set must be a 2-dimensional array.'
     with pytest.raises(IncorrectShapeError) as exin:
-        ftms._input_is_valid(np.array([0,]), None, None, None)
+        ftms._validate_input(
+            np.array([0, ]), None, None, None)  # yapf: disable
     assert str(exin.value) == msg
 
-    msg = ('The input dataset must only contain base types (textual and '
-           'numerical).')
+    msg = ('The input data set must only contain base types (strings and '
+           'numbers).')
     with pytest.raises(ValueError) as exin:
-        ftms._input_is_valid(NOT_BASE_NP_ARRAY, None, None, None)
+        ftms._validate_input(NOT_BASE_NP_ARRAY, None, None, None)
     assert str(exin.value) == msg
 
-    msg = ('sample_size must be an integer or None.')
+    msg = 'sample_size must be an integer.'
     with pytest.raises(TypeError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, 'a', None)
+        ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 'int', None)
     assert str(exin.value) == msg
 
-    msg = ('sample_size must be a positive integer or None.')
+    msg = 'sample_size must be a non-negative integer.'
     with pytest.raises(ValueError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, -1, None)
+        ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, -1, None)
     assert str(exin.value) == msg
 
-    msg = ('num_explanations must be an integer or None.')
+    msg = 'explanations_number must be an integer.'
     with pytest.raises(TypeError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, 1, 'a')
+        ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 1, 'a')
     assert str(exin.value) == msg
 
-    msg = ('num_explanations must be a positive integer or None.')
+    msg = 'explanations_number must be a non-negative integer.'
     with pytest.raises(ValueError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, 1, -1)
+        ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 1, -1)
     assert str(exin.value) == msg
 
-    msg = ('explainer object must be method \'explain_instance\' which has '
-           'exactly one required parameter. Other named parameters can be '
-           'passed to the submodular pick method.')
-    with pytest.raises(IncompatibleExplainerError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, fake_explainer, 1, 1)
+    msg = ('The explain_instance should be a Python callable '
+           '(function or method).')
+    with pytest.raises(TypeError) as exin:
+        ftms._validate_input(NUMERICAL_NP_ARRAY, None, 1, 1)
     assert str(exin.value) == msg
 
-    msg = ('sample_size must be larger or equal to num_explanations.')
+    msg = ('The explain_instance callable must accept '
+           'exactly one required parameter.')
+    cal = lambda x, y: x + y  # noqa: E731
+    with pytest.raises(RuntimeError) as exin:
+        ftms._validate_input(NUMERICAL_NP_ARRAY, cal, 1, 1)
+    assert str(exin.value) == msg
+
+    msg = ('The number of explanations cannot be larger than '
+           'the number of samples.')
     with pytest.raises(ValueError) as exin:
-        ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, 1, 2)
+        ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 1, 2)
     assert str(exin.value) == msg
-    # All ok
-    assert ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, 1, 1)
-    assert ftms._input_is_valid(CATEGORICAL_STRUCT_ARRAY, working_explainer,
-                                None, None)
-    assert ftms._input_is_valid(NUMERICAL_NP_ARRAY, working_explainer, None, 1)
+
+    # All OK
+    assert ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 1, 1)
+    assert ftms._validate_input(
+        CATEGORICAL_STRUCT_ARRAY, explain_instance, 0, 0)  # yapf: disable
+    assert ftms._validate_input(NUMERICAL_NP_ARRAY, explain_instance, 0, 1)
 
 
 def test_submodular_pick():
-    """
-    Tests :func:`fatf.transparency.models.submodular_pick`.
-    """
+    """Tests :func:`fatf.transparency.models.submodular_pick`."""
     fatf.setup_random_seed()
 
-    explainer = DummyExplainer1(NUMERICAL_NP_ARRAY)
     explanations, explanation_ind = ftms.submodular_pick(
-        NUMERICAL_NP_ARRAY, explainer, num_explanations=2)
+        NUMERICAL_NP_ARRAY, explain_instance_a, explanations_number=2)
     assert explanation_ind == [0, 2]
     assert explanations == [EXPLAINERS[0], EXPLAINERS[2]]
 
-    explainer = DummyExplainer2(NUMERICAL_NP_ARRAY)
     explanations, explanation_ind = ftms.submodular_pick(
-        NUMERICAL_NP_ARRAY, explainer, num_explanations=2)
+        NUMERICAL_NP_ARRAY, explain_instance_b, explanations_number=2)
     assert explanation_ind == [0, 1]
     assert explanations == [EXPLAINERS[3], EXPLAINERS[2]]
 
-    explainer = DummyExplainer1(NUMERICAL_NP_ARRAY)
-    msg = ('sample_size is larger than the number of sampels in the dataset. '
+    msg = ('sample_size is larger than the number of samples in the data set. '
            'The whole dataset will be used.')
     with pytest.warns(UserWarning) as warning:
         explanations, explanation_ind = ftms.submodular_pick(
-            NUMERICAL_NP_ARRAY, explainer, sample_size=100, num_explanations=1)
+            NUMERICAL_NP_ARRAY,
+            explain_instance_a,
+            sample_size=100,
+            explanations_number=1)
     assert len(warning) == 1
-    assert str(warning[0].message)
+    assert str(warning[0].message) == msg
     assert explanation_ind == [0]
     assert explanations == [EXPLAINERS[0]]
 
-    explainer = DummyExplainer1(NUMERICAL_NP_ARRAY)
     explanations, explanation_ind = ftms.submodular_pick(
-        NUMERICAL_NP_ARRAY, explainer, sample_size=1, num_explanations=1)
-    assert explanation_ind == [2]
-    assert explanations == [EXPLAINERS[2]]
+        NUMERICAL_NP_ARRAY,
+        explain_instance_a,
+        sample_size=1,
+        explanations_number=1)
+    assert explanation_ind == [1]
+    assert explanations == [EXPLAINERS[1]]
 
-    explainer = DummyExplainer1(NUMERICAL_NP_ARRAY)
     explanations, explanation_ind = ftms.submodular_pick(
-        NUMERICAL_NP_ARRAY, explainer, sample_size=None, num_explanations=None)
+        NUMERICAL_NP_ARRAY,
+        explain_instance_a,
+        sample_size=0,
+        explanations_number=0)
     assert explanation_ind == [0, 2, 1, 3]
-    assert explanations == [EXPLAINERS[0], EXPLAINERS[2], EXPLAINERS[1], 
-                            EXPLAINERS[3]]
+    assert explanations == [
+        EXPLAINERS[0], EXPLAINERS[2], EXPLAINERS[1], EXPLAINERS[3]
+    ]
 
-    explainer = DummyExplainer1(NUMERICAL_NP_ARRAY)
     explanations, explanation_ind = ftms.submodular_pick(
-        NUMERICAL_NP_ARRAY, explainer, sample_size=2, num_explanations=None)
-    assert explanation_ind == [3, 0]
-    assert explanations == [EXPLAINERS[3], EXPLAINERS[0]]
+        NUMERICAL_NP_ARRAY,
+        explain_instance_a,
+        sample_size=2,
+        explanations_number=0)
+    assert explanation_ind == [3, 1]
+    assert explanations == [EXPLAINERS[3], EXPLAINERS[1]]
+
+    msg = ('The number of explanations cannot be larger than '
+           'the number of instances (rows) in the data set.')
+    with pytest.warns(UserWarning) as warning:
+        explanations, explanation_ind = ftms.submodular_pick(
+            NUMERICAL_NP_ARRAY, explain_instance_a, 0, 222)
+    assert len(warning) == 1
+    assert str(warning[0].message) == msg
+    assert explanation_ind == [0, 2, 1, 3]
+    assert explanations == [
+        EXPLAINERS[0], EXPLAINERS[2], EXPLAINERS[1], EXPLAINERS[3]
+    ]
